6d2e6c53c49d8505c0d78787f28c0f6ee7721745
Tim <45952112+heiheisbeak@users.noreply.github.com>
2024-01-12 21:10:27 +0000

    temp disable hooks & remove base url as handled in page class

--- wdio.conf.js
+++ wdio.conf.js
@@ -84,7 +84,7 @@ export const config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'https://demo.nopcommerce.com',
+    //baseUrl: 'https://demo.nopcommerce.com',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
@@ -128,7 +128,8 @@ export const config = {
     // If you are using Cucumber you need to specify the location of your step definitions.
     cucumberOpts: {
         // <string[]> (file/dir) require files before executing features
-        require: ['./features/step-definitions/*.js', './features/hooks.js'],
+        //require: ['./features/step-definitions/*.js', './features/hooks.js'],
+        require: ['./features/step-definitions/*.js'],
         // <boolean> show full backtrace for errors
         backtrace: false,
         // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
