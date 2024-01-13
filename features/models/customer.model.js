3215e708f3904f7d894a29b55f4304e2e9c70422
Tim <45952112+heiheisbeak@users.noreply.github.com>
2024-01-13 16:40:59 +0000

    add more properties to customer model

--- features/models/customer.model.js
+++ features/models/customer.model.js
@@ -10,7 +10,12 @@ class CustomerModel {
         Email = faker.internet.email(),
         CompanyName = faker.company.name(),
         Newsletter = faker.datatype.boolean(),
-
+        Country = faker.location.country(),
+        State = faker.location.state(),
+        City = faker.location.city(),
+        AddressLine1 = faker.location.street(),
+        PostCode = faker.location.zipCode(),
+        Phone = faker.phone.number()
     )
     {
         this.Gender = Gender;
@@ -19,6 +24,12 @@ class CustomerModel {
         this.Email = Email;
         this.CompanyName = CompanyName;
         this.Newsletter = Newsletter;
+        this.Country = Country;
+        this.State = State;
+        this.City = City;
+        this.AddressLine1 = AddressLine1;
+        this.PostCode = PostCode;
+        this.Phone = Phone;
     }
 }
 export default new CustomerModel();
